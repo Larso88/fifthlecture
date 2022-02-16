8d5ce5ed2fb781e4958327b3b4693dac5cafbf9c
Lars Ole Algrøy <Lars_ole_algroy@outlook.com>
2022-02-16 13:03:10 +0100

    Made simple express application with some routes

--- server/server.js
+++ server/server.js
@@ -3,7 +3,11 @@ import express from "express";
 const app = express();
 
 app.get("/login", (req, res,next) => {
-  res.json( {"username": "Noen Andre Person"})
+  res.json( {"username": "Noen Andre Person"});
 })
 
+
+app.post( "/login", (req, res, next) => {
+  res.sendStatus(401);
+})
 app.listen(3000);
