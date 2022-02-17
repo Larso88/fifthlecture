61df18e37597c5df208acecc1259596af16f34f1
Lars Ole Algrøy <Lars_ole_algroy@outlook.com>
2022-02-17 15:11:42 +0100

    Program now uses port from environment. Need to find out why .port does not work

--- server/server.js
+++ server/server.js
@@ -10,4 +10,6 @@ app.get("/login", (req, res,next) => {
 app.post( "/login", (req, res, next) => {
   res.sendStatus(401);
 })
-app.listen(3000);
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server started on http://localhost:${server.address().port}`)
+});
